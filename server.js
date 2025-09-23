298e4da57dd1a664cfb24f9460db2e40c12f5283
Shah Zaib <144054775+ShahZaib151214@users.noreply.github.com>
2025-09-23 19:38:03 +0500

    hello world

--- server.js
+++ server.js
@@ -148,6 +148,9 @@ app.use("/api", cashierRouter);
 app.use("/api", supervisorRouter);
 app.use("/api", resetPasswordRouter);
 app.use("/api", currencyRouter);
+app.use("/",(req,res)=>{
+  res.send("Hello World");
+});
 
 // Schedule: Run every day at 12 AM Pakistan time
 cron.schedule(
